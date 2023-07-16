48e80678706678c8061f29e2ffb3f0d5749ce9bb
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-07-16 20:51:06 -0300

    txt read

--- src/components/dropZone.tsx
+++ src/components/dropZone.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import Dropzone from 'react-dropzone';
+import React, { useCallback } from 'react';
+import Dropzone, { useDropzone } from 'react-dropzone';
 import Box from '@mui/material/Box';
 
 export default function DropZone() {
+  const onDrop = useCallback((acceptedFiles: any) => {
+    acceptedFiles.forEach((file: any) => {
+      const reader = new FileReader();
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = (e: any) => {
+        const text = e.target.result;
+        console.log(text);
+      };
+      reader.readAsText(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
   return (
-    <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
-      {({ getRootProps, getInputProps }) => (
-        <section>
-          <div {...getRootProps()}>
-            <Box component="span" sx={{ p: 15, border: '1px dashed grey' }}>
-              <input {...getInputProps()} />
-              <label>
-                <b>Choose a file</b> or drag it here.
-              </label>
-            </Box>
-          </div>
-        </section>
-      )}
-    </Dropzone>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+    // <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+    //   {({ getRootProps, getInputProps }) => (
+    //     <section>
+    //       <div {...getRootProps()}>
+    //         <Box component="span" sx={{ p: 15, border: '1px dashed grey' }}>
+    //           <input {...getInputProps()} />
+    //           <label>
+    //             <b>Choose a file</b> or drag it here.
+    //           </label>
+    //         </Box>
+    //       </div>
+    //     </section>
+    //   )}
+    // </Dropzone>
   );
 }
