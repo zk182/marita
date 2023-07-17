24a689577764b01ce584fff333a2fe2aa07353f7
Nicolas Battaglia <nicobattmdq@gmail.com>
2023-07-16 21:50:55 -0300

    Data passed to parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 import DenseTable from './components/table';
 import DropZone from './components/dropZone';
 import { Container } from '@mui/material';
 
 function App() {
-  const show = true;
+  const [showDrop, setShowDrop] = useState(true);
+  const [data, setData] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        <DropZone />
-        {/* <Container>{show && <DenseTable />}</Container> */}
+        {showDrop && (
+          <DropZone
+            showData={(d: string) => {
+              setData(d);
+              setShowDrop(false);
+            }}
+          />
+        )}
+        {!showDrop && (
+          <Container>
+            <DenseTable />
+          </Container>
+        )}
       </header>
     </div>
   );
--- src/components/dropZone.tsx
+++ src/components/dropZone.tsx
@@ -1,39 +1,35 @@
-import React, { useCallback } from 'react';
-import Dropzone, { useDropzone } from 'react-dropzone';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
 import Box from '@mui/material/Box';
 
-export default function DropZone() {
+export default function DropZone(props: any) {
   const onDrop = useCallback((acceptedFiles: any) => {
     acceptedFiles.forEach((file: any) => {
+      let text = '';
       const reader = new FileReader();
       reader.onabort = () => console.log('file reading was aborted');
       reader.onerror = () => console.log('file reading has failed');
+      reader.onloadend = () => {
+        console.log('finished');
+        props.showData(text);
+      };
       reader.onload = (e: any) => {
-        const text = e.target.result;
-        console.log(text);
+        text = e.target.result;
       };
       reader.readAsText(file);
     });
   }, []);
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
-    </div>
-    // <Dropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
-    //   {({ getRootProps, getInputProps }) => (
-    //     <section>
-    //       <div {...getRootProps()}>
-    //         <Box component="span" sx={{ p: 15, border: '1px dashed grey' }}>
-    //           <input {...getInputProps()} />
-    //           <label>
-    //             <b>Choose a file</b> or drag it here.
-    //           </label>
-    //         </Box>
-    //       </div>
-    //     </section>
-    //   )}
-    // </Dropzone>
+    <section>
+      <div {...getRootProps()}>
+        <Box component="span" sx={{ p: 15, border: '1px dashed grey' }}>
+          <input {...getInputProps()} />
+          <label>
+            <b>Choose a file</b> or drag it here. //{' '}
+          </label>
+        </Box>
+      </div>
+    </section>
   );
 }
